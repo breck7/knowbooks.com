5345ec4cc9634d78f9491f79d31d1a6faf8b827c
breck7 <breck7@gmail.com>
2026-08-23 04:25:04 +0000

    Deploying to wws from @ breck7/knowbooks.com@52eb9175398c590564137830dfa71b6eabd97348 🚀

---
+++ catalog.js
@@ -0,0 +1,146 @@
+const catalog = [
+  {
+    "id": "magnets",
+    "title": "Magnets",
+    "status": "beta",
+    "priceUsd": 49
+  },
+  {
+    "id": "beams",
+    "title": "Beams",
+    "status": "beta",
+    "priceUsd": 49
+  },
+  {
+    "id": "focus",
+    "title": "Focus",
+    "status": "beta",
+    "priceUsd": 99
+  },
+  {
+    "id": "light",
+    "title": "Light",
+    "status": "beta",
+    "priceUsd": 59
+  },
+  {
+    "id": "capital",
+    "title": "Capital",
+    "status": "beta",
+    "priceUsd": 399
+  },
+  {
+    "id": "levers",
+    "title": "Levers",
+    "status": "beta",
+    "priceUsd": 49
+  },
+  {
+    "id": "gravity",
+    "title": "Gravity",
+    "status": "beta",
+    "priceUsd": 59
+  },
+  {
+    "id": "sound",
+    "title": "Sound",
+    "status": "beta",
+    "priceUsd": 69
+  },
+  {
+    "id": "electricity",
+    "title": "Electricity",
+    "status": "beta",
+    "priceUsd": 79
+  },
+  {
+    "id": "air",
+    "title": "Air",
+    "status": "beta",
+    "priceUsd": 69
+  },
+  {
+    "id": "heat",
+    "title": "Heat",
+    "status": "beta",
+    "priceUsd": 79
+  },
+  {
+    "id": "glass",
+    "title": "Glass",
+    "status": "beta",
+    "priceUsd": 199
+  },
+  {
+    "id": "cells",
+    "title": "Cells",
+    "status": "beta",
+    "priceUsd": 299
+  },
+  {
+    "id": "iron",
+    "title": "Iron",
+    "status": "beta",
+    "priceUsd": 129
+  },
+  {
+    "id": "elements",
+    "title": "Elements",
+    "status": "beta",
+    "priceUsd": 199
+  },
+  {
+    "id": "copper",
+    "title": "Copper",
+    "status": "beta",
+    "priceUsd": 129
+  },
+  {
+    "id": "fire",
+    "title": "Fire",
+    "status": "beta",
+    "priceUsd": 129
+  },
+  {
+    "id": "sleep",
+    "title": "Sleep",
+    "status": "beta",
+    "priceUsd": 499
+  },
+  {
+    "id": "fields",
+    "title": "Fields",
+    "status": "beta",
+    "priceUsd": 199
+  },
+  {
+    "id": "hydration",
+    "title": "Hydration",
+    "status": "beta",
+    "priceUsd": 199
+  },
+  {
+    "id": "food",
+    "title": "Food",
+    "status": "beta",
+    "priceUsd": 149
+  },
+  {
+    "id": "software",
+    "title": "Software",
+    "status": "beta",
+    "priceUsd": 599
+  },
+  {
+    "id": "sales",
+    "title": "Sales",
+    "status": "beta",
+    "priceUsd": 399
+  },
+  {
+    "id": "twitter",
+    "title": "Twitter",
+    "status": "beta",
+    "priceUsd": 799
+  }
+]
