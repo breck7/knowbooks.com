4583fee667b12266954e910e32779b3d75759e2d
breck7 <breck7@gmail.com>
2026-09-02 19:23:07 +0000

    Deploying to wws from @ breck7/knowbooks.com@fd90b82efcc9718dde9b0cbcca1c0071d3a71508 🚀

--- catalog.js
+++ catalog.js
@@ -2,367 +2,385 @@ const catalog = [
   {
     "id": "magnets",
     "title": "Magnets",
-    "status": "beta",
+    "status": "trailblazer",
     "priceUsd": 49
   },
   {
     "id": "beams",
     "title": "Beams",
-    "status": "beta",
+    "status": "trailblazer",
     "priceUsd": 49
   },
   {
     "id": "focus",
     "title": "Focus",
-    "status": "beta",
-    "priceUsd": 99
-  },
-  {
-    "id": "light",
-    "title": "Light",
-    "status": "beta",
-    "priceUsd": 59
+    "status": "trailblazer",
+    "priceUsd": 49
   },
   {
     "id": "capital",
     "title": "Capital",
-    "status": "beta",
-    "priceUsd": 399
+    "status": "trailblazer",
+    "priceUsd": 999
+  },
+  {
+    "id": "light",
+    "title": "Light",
+    "status": "believer",
+    "priceUsd": 99
   },
   {
     "id": "levers",
     "title": "Levers",
-    "status": "beta",
-    "priceUsd": 49
+    "status": "believer",
+    "priceUsd": 99
   },
   {
     "id": "carpentry",
     "title": "Carpentry",
-    "status": "beta",
+    "status": "believer",
     "priceUsd": 149
   },
   {
     "id": "gravity",
     "title": "Gravity",
-    "status": "beta",
-    "priceUsd": 59
+    "status": "believer",
+    "priceUsd": 99
   },
   {
     "id": "sound",
     "title": "Sound",
-    "status": "beta",
-    "priceUsd": 69
+    "status": "believer",
+    "priceUsd": 99
   },
   {
     "id": "electricity",
     "title": "Electricity",
-    "status": "beta",
-    "priceUsd": 79
+    "status": "believer",
+    "priceUsd": 99
   },
   {
     "id": "air",
     "title": "Air",
-    "status": "beta",
-    "priceUsd": 69
+    "status": "believer",
+    "priceUsd": 99
   },
   {
     "id": "heat",
     "title": "Heat",
-    "status": "beta",
-    "priceUsd": 79
+    "status": "believer",
+    "priceUsd": 99
   },
   {
     "id": "glass",
     "title": "Glass",
-    "status": "beta",
+    "status": "believer",
     "priceUsd": 199
   },
   {
     "id": "cells",
     "title": "Cells",
-    "status": "beta",
+    "status": "believer",
     "priceUsd": 299
   },
   {
     "id": "iron",
     "title": "Iron",
-    "status": "beta",
+    "status": "believer",
     "priceUsd": 129
   },
   {
     "id": "elements",
     "title": "Elements",
-    "status": "beta",
+    "status": "believer",
     "priceUsd": 199
   },
   {
     "id": "copper",
     "title": "Copper",
-    "status": "beta",
+    "status": "believer",
     "priceUsd": 129
   },
   {
     "id": "fire",
     "title": "Fire",
-    "status": "beta",
+    "status": "believer",
     "priceUsd": 129
   },
   {
     "id": "sleep",
     "title": "Sleep",
-    "status": "beta",
+    "status": "believer",
     "priceUsd": 499
   },
   {
     "id": "fields",
     "title": "Fields",
-    "status": "beta",
+    "status": "believer",
     "priceUsd": 199
   },
   {
     "id": "energy",
     "title": "Energy",
-    "status": "beta",
+    "status": "believer",
     "priceUsd": 139
   },
   {
     "id": "voltage",
     "title": "Voltage",
-    "status": "beta",
+    "status": "believer",
     "priceUsd": 99
   },
   {
     "id": "motors",
     "title": "Motors",
-    "status": "beta",
+    "status": "believer",
     "priceUsd": 199
   },
   {
     "id": "gears",
     "title": "Gears",
-    "status": "beta",
+    "status": "believer",
     "priceUsd": 149
   },
   {
     "id": "colors",
     "title": "Colors",
-    "status": "beta",
-    "priceUsd": 89
-  },
-  {
-    "id": "gluons",
-    "title": "Gluons",
-    "status": "beta",
-    "priceUsd": 899
-  },
-  {
-    "id": "bosons",
-    "title": "Bosons",
-    "status": "beta",
-    "priceUsd": 899
+    "status": "believer",
+    "priceUsd": 99
   },
   {
     "id": "matter",
     "title": "Matter",
-    "status": "beta",
+    "status": "believer",
     "priceUsd": 199
   },
   {
     "id": "atoms",
     "title": "Atoms",
-    "status": "beta",
+    "status": "believer",
     "priceUsd": 199
   },
   {
     "id": "motion",
     "title": "Motion",
-    "status": "beta",
+    "status": "believer",
     "priceUsd": 99
   },
   {
     "id": "friction",
     "title": "Friction",
-    "status": "beta",
-    "priceUsd": 59
+    "status": "believer",
+    "priceUsd": 99
   },
   {
     "id": "ropes",
     "title": "Ropes",
-    "status": "beta",
-    "priceUsd": 79
+    "status": "believer",
+    "priceUsd": 99
   },
   {
     "id": "wind",
     "title": "Wind",
-    "status": "beta",
-    "priceUsd": 59
+    "status": "believer",
+    "priceUsd": 99
   },
   {
     "id": "water",
     "title": "Water",
-    "status": "beta",
-    "priceUsd": 89
+    "status": "believer",
+    "priceUsd": 99
   },
   {
     "id": "rust",
     "title": "Rust",
-    "status": "beta",
+    "status": "believer",
+    "priceUsd": 99
+  },
+  {
+    "id": "time",
+    "title": "Time",
+    "status": "believer",
     "priceUsd": 99
   },
   {
     "id": "liberty",
     "title": "Liberty",
-    "status": "beta",
+    "status": "believer",
     "priceUsd": 199
   },
   {
     "id": "courage",
     "title": "Courage",
-    "status": "beta",
+    "status": "believer",
     "priceUsd": 199
   },
   {
     "id": "property",
     "title": "Property",
-    "status": "beta",
+    "status": "believer",
     "priceUsd": 399
   },
   {
     "id": "membranes",
     "title": "Membranes",
-    "status": "beta",
+    "status": "believer",
     "priceUsd": 199
   },
   {
     "id": "decay",
     "title": "Decay",
-    "status": "beta",
+    "status": "believer",
     "priceUsd": 299
   },
   {
     "id": "bonds",
     "title": "Bonds",
-    "status": "beta",
+    "status": "believer",
     "priceUsd": 159
   },
   {
     "id": "teamwork",
     "title": "Teamwork",
-    "status": "beta",
+    "status": "believer",
     "priceUsd": 199
   },
   {
     "id": "patience",
     "title": "Patience",
-    "status": "beta",
+    "status": "believer",
     "priceUsd": 149
   },
   {
     "id": "paper",
     "title": "Paper",
-    "status": "beta",
+    "status": "believer",
     "priceUsd": 99
   },
   {
     "id": "ink",
     "title": "Ink",
-    "status": "beta",
+    "status": "believer",
     "priceUsd": 99
   },
   {
     "id": "metals",
     "title": "Metals",
-    "status": "beta",
+    "status": "believer",
     "priceUsd": 129
   },
   {
     "id": "steel",
     "title": "Steel",
-    "status": "beta",
+    "status": "believer",
     "priceUsd": 119
   },
   {
     "id": "aluminum",
     "title": "Aluminum",
-    "status": "beta",
+    "status": "believer",
     "priceUsd": 109
   },
   {
     "id": "brass",
     "title": "Brass",
-    "status": "beta",
+    "status": "believer",
     "priceUsd": 99
   },
   {
     "id": "wood",
     "title": "Wood",
-    "status": "beta",
-    "priceUsd": 89
+    "status": "believer",
+    "priceUsd": 99
   },
   {
     "id": "wheels",
     "title": "Wheels",
-    "status": "beta",
-    "priceUsd": 79
+    "status": "believer",
+    "priceUsd": 99
   },
   {
     "id": "stars",
     "title": "Stars",
-    "status": "beta",
+    "status": "believer",
     "priceUsd": 99
   },
   {
     "id": "exploration",
     "title": "Exploration",
-    "status": "beta",
+    "status": "believer",
     "priceUsd": 199
   },
   {
     "id": "fabric",
     "title": "Fabric",
-    "status": "beta",
+    "status": "believer",
     "priceUsd": 179
-  },
-  {
-    "id": "mitochondria",
-    "title": "Mitochondria",
-    "status": "beta",
-    "priceUsd": 499
-  },
-  {
-    "id": "hydration",
-    "title": "Hydration",
-    "status": "beta",
-    "priceUsd": 199
   },
   {
     "id": "food",
     "title": "Food",
-    "status": "beta",
+    "status": "believer",
     "priceUsd": 149
   },
   {
     "id": "software",
     "title": "Software",
-    "status": "beta",
+    "status": "believer",
     "priceUsd": 599
   },
   {
     "id": "scroll",
     "title": "Scroll",
-    "status": "beta",
+    "status": "believer",
     "priceUsd": 499
   },
   {
     "id": "sales",
     "title": "Sales",
-    "status": "beta",
+    "status": "believer",
     "priceUsd": 399
   },
   {
     "id": "twitter",
     "title": "Twitter",
-    "status": "beta",
-    "priceUsd": 799
+    "status": "believer",
+    "priceUsd": 999
+  },
+  {
+    "id": "hydration",
+    "title": "Hydration",
+    "status": "believer",
+    "priceUsd": 199
+  },
+  {
+    "id": "actuators",
+    "title": "Actuators",
+    "status": "frontier",
+    "priceUsd": 999
+  },
+  {
+    "id": "transistors",
+    "title": "Transistors",
+    "status": "frontier",
+    "priceUsd": 999
+  },
+  {
+    "id": "mitochondria",
+    "title": "Mitochondria",
+    "status": "frontier",
+    "priceUsd": 999
+  },
+  {
+    "id": "gluons",
+    "title": "Gluons",
+    "status": "frontier",
+    "priceUsd": 999
+  },
+  {
+    "id": "bosons",
+    "title": "Bosons",
+    "status": "frontier",
+    "priceUsd": 999
   }
 ]
